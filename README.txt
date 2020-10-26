af46ac886dd8097ec3b59bb281890f5696976834
Hector Miguel Arellano Aquino <hector.mikel@gmail.com>
2020-10-26 21:27:15 +0800

    Initial commit

---
+++ README.txt
@@ -0,0 +1,66 @@
+# Singapore Income Tax Rates
+
+**Version 1.0.0**
+
+This program calculates the income tax due for individuals who have worked in Singapore. This refers to the information published as of July 2020 on the Inland Revenue Authority of Singapore’s website: https://www.iras.gov.sg/irashome/Individuals/Locals/Working-Out-Your-Taxes/Income-Tax-Rates/.  
+The user must declare whether he is a Singaporean citizen, Permanent Resident, or a foreigner employed in Singapore for at least 183 days. After which, user inputs his gross income for the tax year. The program returns the user’s tax amount due and the net income (gross income less income tax).
+Taken from the website:
+
+Income Tax Rates
+ 
+
+Income tax rates depend on an individual's tax residency status. You will be treated as a tax resident for a particular Year of Assessment (YA) if you are a: 
+a.	Singapore Citizen (SC) or Singapore Permanent Resident (SPR) who resides in Singapore except for temporary absences; or
+b.	Foreigner who has stayed / worked in Singapore (excludes director of a company) for 183 days or more in the year preceding the YA.
+Otherwise, you will be treated as a non-resident of Singapore for tax purposes.
+Singapore's personal income tax rates for resident taxpayers are progressive. This means higher income earners pay a proportionately higher tax, with the current highest personal income tax rate at 22%.
+BACK TO TOP
+Resident Tax Rates
+From YA 2017 onwards
+Chargeable Income	Income Tax Rate (%)	Gross Tax Payable ($)
+First $20,000
+Next $10,000	0
+2	0
+200
+First $30,000
+Next $10,000	-
+3.50	200
+350
+First $40,000
+Next $40,000	-
+7	550
+2,800
+First $80,000
+Next $40,000	-
+11.5	3,350
+4,600
+First $120,000
+Next $40,000	-
+15	7,950
+6,000
+First $160,000
+Next $40,000	-
+18	13,950
+7,200
+First $200,000
+Next $40,000	-
+19	21,150
+7,600
+First $240,000
+Next $40,000	-
+19.5	28,750
+7,800
+First $280,000
+Next $40,000	-
+20	36,550
+8,000
+First $320,000
+In excess of $320,000	-
+22	44,550
+
+_ _ _
+
+## Coded by
+
+Mikkel Aquino
+Full name: Hector Miguel Arellano Aquino
---
+++ SGIncomeTax/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+
+namespace SGIncomeTax
+{
+    class Program
+    {
+        /*
+         Singpaore Income Tax Calculator can be found here:
+         https://www.iras.gov.sg/irashome/Individuals/Locals/Working-Out-Your-Taxes/Income-Tax-Rates/
+         */
+        static void Main(string[] args)
+        {
+            string resident;
+            bool needToPayTax = true;
+
+            Console.WriteLine("Hello! Welcome to the Singapore Income Tax Calculator for employees!");
+            while (needToPayTax == true)
+            {
+                Console.WriteLine("\nAre you a Singapore citizen, Singapore PR, or a foreigner employed in Singapore for at least 183 days? (yes/no)");
+                resident = Console.ReadLine();
+                resident.ToLower();
+                if (resident == "yes" || resident == "y")
+                {
+                    ResidentTaxCalculation(); // run method for residents
+                    needToPayTax = false; // break out of while loop
+                }
+                else if (resident == "no" || resident == "n")
+                {
+                    NonResidentTaxCalculation(); // run method for non-residents
+                    needToPayTax = false; // break out of while loop 
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please input yes or no.");
+                    needToPayTax = true;
+                }
+            }
+        }
+        // ** METHODS() **
+
+        // A. create nonResidentTaxCalculation() method
+        static void NonResidentTaxCalculation()
+        {
+            double grossIncome;
+            double flatTaxRate = 0.15; // ** see how to make this a constant later; unchangeable
+            double taxAmountDue;
+            double netIncome;
+
+            Console.WriteLine("Please input your gross income for the year.");
+            bool parsedSuccessfully = double.TryParse(Console.ReadLine(), out grossIncome);
+
+            if (parsedSuccessfully == true)
+            {
+                taxAmountDue = flatTaxRate * grossIncome;
+                netIncome = grossIncome - taxAmountDue;
+            }
+            else
+            {
+                while (parsedSuccessfully == false)
+                {
+                    Console.WriteLine("Please input a number only.");
+                    parsedSuccessfully = double.TryParse(Console.ReadLine(), out grossIncome);
+                }
+                taxAmountDue = flatTaxRate * grossIncome;
+                netIncome = grossIncome - taxAmountDue;
+            }
+            Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+        }
+        // B. create ResidentTaxCalculation() method
+        static void ResidentTaxCalculation()
+        {
+            double grossIncome;
+            double taxAmountDue;
+            double netIncome;
+
+            Console.WriteLine("Please input your gross income for the year.");
+            bool parsedSuccessfully = double.TryParse(Console.ReadLine(), out grossIncome);
+
+            if (parsedSuccessfully == true)
+            {
+                if (grossIncome <= 20000)
+                {
+                    taxAmountDue = 20000 * 0;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome <= 30000)
+                {
+                    taxAmountDue = 20000 * 0 + (grossIncome - 20000) * 0.02;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome <= 40000)
+                {
+                    taxAmountDue = 200 + (grossIncome - 30000) * 0.035;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome <= 80000)
+                {
+                    taxAmountDue = 550 + (grossIncome - 40000) * 0.07;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome <= 120000)
+                {
+                    taxAmountDue = 3350 + (grossIncome - 80000) * 0.115;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome <= 160000)
+                {
+                    taxAmountDue = 7950 + (grossIncome - 120000) * 0.15;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome <= 200000)
+                {
+                    taxAmountDue = 13950 + (grossIncome - 160000) * 0.18;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome <= 240000)
+                {
+                    taxAmountDue = 21150 + (grossIncome - 200000) * 0.19;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome <= 280000)
+                {
+                    taxAmountDue = 28750 + (grossIncome - 240000) * 0.195;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome <= 320000)
+                {
+                    taxAmountDue = 36550 + (grossIncome - 280000) * 0.2;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome >= 320000)
+                {
+                    taxAmountDue = 44550 + (grossIncome - 320000) * 0.22;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+            }
+            else // I.e., user did not input a number for his annual salary. parsedSuccessfully == false.
+            {
+                while (parsedSuccessfully == false)
+                {
+                    Console.WriteLine("Please input a number only.");
+                    parsedSuccessfully = double.TryParse(Console.ReadLine(), out grossIncome);
+                }
+                if (grossIncome <= 20000)
+                {
+                    taxAmountDue = 20000 * 0;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome <= 30000)
+                {
+                    taxAmountDue = 20000 * 0 + (grossIncome - 20000) * 0.02;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome <= 40000)
+                {
+                    taxAmountDue = 200 + (grossIncome - 30000) * 0.035;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome <= 80000)
+                {
+                    taxAmountDue = 550 + (grossIncome - 40000) * 0.07;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome <= 120000)
+                {
+                    taxAmountDue = 3350 + (grossIncome - 80000) * 0.115;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome <= 160000)
+                {
+                    taxAmountDue = 7950 + (grossIncome - 120000) * 0.15;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome <= 200000)
+                {
+                    taxAmountDue = 13950 + (grossIncome - 160000) * 0.18;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome <= 240000)
+                {
+                    taxAmountDue = 21150 + (grossIncome - 200000) * 0.19;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome <= 280000)
+                {
+                    taxAmountDue = 28750 + (grossIncome - 240000) * 0.195;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome <= 320000)
+                {
+                    taxAmountDue = 36550 + (grossIncome - 280000) * 0.2;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+                else if (grossIncome >= 320000)
+                {
+                    taxAmountDue = 44550 + (grossIncome - 320000) * 0.22;
+                    netIncome = grossIncome - taxAmountDue;
+                    Console.WriteLine("\nYour tax amount due is {0} and your net income is {1}", taxAmountDue, netIncome);
+                }
+            }
+        }
+    }
+}
